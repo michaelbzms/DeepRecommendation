71d3ad10317ab74c4aef9b1ce05c00d4d5fb8f63
Michael <mikemikebzms@gmail.com>
2021-12-16 11:47:47 +0200

    WIP: Multiple GCN Convolutions!

--- src/gnns/models/GCN.py
+++ src/gnns/models/GCN.py
@@ -8,22 +8,25 @@ from neural_collaborative_filtering.util import build_MLP_layers
 
 
 class GCN_NCF(GNN_NCF):
-    def __init__(self, gnn_hidden=256, item_emb=128, user_emb=128, mlp_dense_layers=None, dropout_rate=0.2):
+    def __init__(self, gnn_hidden_layers=None, item_emb=128, user_emb=128, mlp_dense_layers=None, dropout_rate=0.2):
         super(GCN_NCF, self).__init__()
         if mlp_dense_layers is None: mlp_dense_layers = [256, 128]         # default
-        self.kwargs = {'gnn_hidden': gnn_hidden,
+        if gnn_hidden_layers is None: gnn_hidden_layers = [256, 128]       # default
+        self.kwargs = {'gnn_hidden_layers': gnn_hidden_layers,
                        'item_emb': item_emb,
                        'user_emb': user_emb,
                        'mlp_dense_layers': mlp_dense_layers}
 
-        self.conv = GCNConv(in_channels=-1, out_channels=gnn_hidden)
+        self.gnn_convs = nn.ModuleList(
+            [GCNConv(in_channels=-1 if i == 0 else gnn_hidden_layers[i-1], out_channels=gnn_hidden_layers[i]) for i in range(len(gnn_hidden_layers))]
+        )
 
         self.item_embeddings = nn.Sequential(
-            nn.Linear(gnn_hidden, item_emb),
+            nn.Linear(gnn_hidden_layers[-1], item_emb),
             nn.ReLU()
         )
         self.user_embeddings = nn.Sequential(
-            nn.Linear(gnn_hidden, user_emb),
+            nn.Linear(gnn_hidden_layers[-1], user_emb),
             nn.ReLU()
         )
         self.MLP = build_MLP_layers(item_emb + user_emb, mlp_dense_layers, dropout_rate=dropout_rate)
@@ -33,8 +36,10 @@ class GCN_NCF(GNN_NCF):
 
     def forward(self, graph, batch):
         # encode with GNN
-        graph_emb = self.conv(graph.x, graph.edge_index, edge_weight=graph.edge_attr)
-        graph_emb = F.leaky_relu(graph_emb)
+        graph_emb = graph.x        # initial representation
+        for gnn_conv in self.gnn_convs:
+            graph_emb = gnn_conv(graph_emb, graph.edge_index, edge_weight=graph.edge_attr)
+            graph_emb = F.leaky_relu(graph_emb)
         print(graph_emb)
 
         # find embeddings of items in batch
