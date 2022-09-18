44030a87f59db1563f29e61dedc354d9e68e181b
michaelbzms <mikemikebzms@gmail.com>
2022-09-18 18:25:17 +0300

    WIP: Attention NCF Backend for web-app TODO: Include explanation

--- src/webapp/backend.py
+++ src/webapp/backend.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 import flask
 from flask import jsonify, request
@@ -5,8 +6,9 @@ import json
 import torch
 
 from globals import item_metadata_file
-from neural_collaborative_filtering.models.basic_ncf import BasicNCF
+from neural_collaborative_filtering.models.advanced_ncf import AttentionNCF
 from neural_collaborative_filtering.util import load_model
+
 
 app = flask.Flask('movie_recommender_backend')
 
@@ -72,32 +74,65 @@ def recommend():
     return response
 
 
-def create_user_profile(item_features: pd.DataFrame, user_ratings: pd.Series):
-    return ((user_ratings.values - user_ratings.mean()).reshape(-1, 1) * item_features.loc[user_ratings.index].values).mean(axis=0)
+# def create_user_profile(item_features: pd.DataFrame, user_ratings: pd.Series):
+#     return ((user_ratings.values - (user_ratings.mean() + 2.5) / 2).reshape(-1, 1) * item_features.loc[user_ratings.index].values).mean(axis=0)
+#
+#
+# def recommend_for_user(model: BasicNCF, item_features: pd.DataFrame, user_ratings: pd.Series,  k, ignore_seen):
+#     # determine items to be forwarded
+#     items_to_use = item_features.drop(user_ratings.index) if ignore_seen else item_features
+#
+#     # prepare item input
+#     item_input = torch.FloatTensor(items_to_use.values).to(device)
+#
+#     # build user profile from user ratings (repeat it once for each item)
+#     user_features = create_user_profile(item_features, user_ratings)
+#     user_input = torch.FloatTensor(user_features.reshape(1, -1).repeat(items_to_use.shape[0], axis=0)).to(device)
+#
+#     # forward the model
+#     with torch.no_grad():
+#         y_pred = model(user_input, item_input).cpu().view(-1).numpy()
+#
+#     # sort the output with their imdb id
+#     predictions = pd.DataFrame(data={
+#         'imdbID': items_to_use.index,
+#         'score': y_pred
+#     }).sort_values(by='score', ascending=False).iloc[:k]
+#
+#     # return sorted imdb_id - predicted score pairs
+#     return predictions
 
 
-def recommend_for_user(model: BasicNCF, item_features: pd.DataFrame, user_ratings: pd.Series,  k, ignore_seen):
-    # determine items to be forwarded
+def recommend_for_user(model: AttentionNCF, item_features: pd.DataFrame, user_ratings: pd.Series,  k, ignore_seen):
+    # candidate item profiles
     items_to_use = item_features.drop(user_ratings.index) if ignore_seen else item_features
+    candidate_items = torch.FloatTensor(items_to_use.values).to(device)                 # (B, F)
 
-    # prepare item input
-    item_input = torch.FloatTensor(items_to_use.values).to(device)
+    # rated items profiles (I, F)
+    rated_items_ids = np.sort(np.unique(user_ratings.index))
+    rated_items = torch.FloatTensor(item_features.loc[rated_items_ids].values).to(device)
 
-    # build user profile from user ratings (repeat it once for each item)
-    user_features = create_user_profile(item_features, user_ratings)
-    user_input = torch.FloatTensor(user_features.reshape(1, -1).repeat(items_to_use.shape[0], axis=0)).to(device)
+    # user_matrix has shape (B, I) but it's for 1 user so it's just the same row B times
+    user_matrix = torch.FloatTensor(np.repeat(np.expand_dims(user_ratings.loc[rated_items_ids].values - ((user_ratings.mean() + 2.5) / 2), axis=0), candidate_items.shape[0], axis=0))
 
-    # forward the model
     with torch.no_grad():
-        y_pred = model(user_input, item_input).cpu().view(-1).numpy()
+        # forward the model
+        y_pred, att_weights = model(candidate_items.float().to(device),
+                                    rated_items.float().to(device),
+                                    user_matrix.float().to(device),
+                                    return_attention_weights=True)
+        # from torch tensors to numpy vectors
+        y_pred = y_pred.view(-1).cpu().numpy()
+        att_weights = att_weights.view(-1).cpu().numpy()
 
     # sort the output with their imdb id
     predictions = pd.DataFrame(data={
         'imdbID': items_to_use.index,
         'score': y_pred
+        # TODO: column with list of why
     }).sort_values(by='score', ascending=False).iloc[:k]
 
-    # return sorted imdb_id - predicted score pairs
+    # return sorted imdb_id and predicted score pairs
     return predictions
 
 
@@ -109,14 +144,14 @@ if __name__ == '__main__':
 
     # load item input to model
     print('Load item features...')
-    metadata: pd.DataFrame = pd.read_hdf('../' + item_metadata_file + '.h5')
+    metadata: pd.DataFrame = pd.read_hdf(f'../{item_metadata_file}.h5')
     item_features = metadata.loc[movie_info.index]
     print('Done.')
 
-    # # load model
+    # load model
     print('Loading model...')
-    model_file = '../../models/ncf_with_features.pt'
-    model = load_model(model_file, BasicNCF)
+    model_file = '../../models/runs/AttentionNCF_with_features_attNet128_3mil.pt'
+    model = load_model(model_file, AttentionNCF)
     model.eval()
     model.to(device)
     print(model)
